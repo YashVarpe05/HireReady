045d9e912ea6f8ff8e12a8e630926f9880c2a4b9
YashVarpe05 <yashvarpe2005@gmail.com>
2026-05-18 02:09:55 +0530

    Refactor code structure and improve API service configurations

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose")
+const express = require("express")
+const app = express()
 
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 async function connectToDB() {
 
