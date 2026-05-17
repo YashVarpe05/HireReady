cca93ef5cfd24f3a6bcffe554e23b58747d342e1
YashVarpe05 <yashvarpe2005@gmail.com>
2026-05-18 02:10:07 +0530

    Refactor code for consistency and improve formatting in app.js and database.js

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,20 +1,18 @@
-const mongoose = require("mongoose")
-const express = require("express")
-const app = express()
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 async function connectToDB() {
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
 
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-
-        console.log("Connected to Database")
-    }
-    catch (err) {
-        console.log(err)
-    }
+		console.log("Connected to Database");
+	} catch (err) {
+		console.log(err);
+	}
 }
 
-module.exports = connectToDB
+module.exports = connectToDB;
